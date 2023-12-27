61bc66eaa82278e586198ce8729c9debb50e3379
GITHUB-Vipul <126069867+GITHUB-Vipul@users.noreply.github.com>
2023-12-27 22:54:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+ var audio;
+function playSound(mp3)
+	{
+	 audio=new Audio(mp3);
+	audio.play();	
+    }
+
+function stop()
+	{
+       audio.pause();	
+    }
+    
